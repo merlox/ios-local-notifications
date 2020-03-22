676b6134e804438f375656629efabee7f914d4f5
merlox <merunasgrincalaitis@gmail.com>
2020-03-22 13:26:40 +0100

    Initial Commit

---
+++ LocalNotifications/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalNotifications
+//
+//  Created by Merunas Grincalaitis on 22/03/2020.
+//  Copyright © 2020 Merunas Grincalaitis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
