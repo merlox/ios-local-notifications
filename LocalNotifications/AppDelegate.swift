07b2661abf2142250c788a9b13333efab2ac6dac
merlox <merunasgrincalaitis@gmail.com>
2020-03-22 17:34:09 +0100

    First commit

--- LocalNotifications/AppDelegate.swift
+++ LocalNotifications/AppDelegate.swift
@@ -5,16 +5,49 @@
 //  Created by Merunas Grincalaitis on 22/03/2020.
 //  Copyright © 2020 Merunas Grincalaitis. All rights reserved.
 //
+// This is the basic UserNotifications example
+// to show you how it's done, no filters
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // 1. Ask for permission
+        let current = UNUserNotificationCenter.current()
+        current.requestAuthorization(options: [.alert, .sound, .badge]) { (granted: Bool, err: Error?) in
+            if !granted {
+                print("Permission NOT granted")
+            } else {
+                print("Permission granted")
+            }
+        }
+        
+        // 2. Create a notification
+        let notification = UNMutableNotificationContent()
+        notification.title = "Notification title"
+        notification.body = "This is my notification"
+        
+        // 3. Trigger the notification every 60 second:
+        // let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60, repeats: true)
+        // Everyday at 8am:
+        var date = DateComponents()
+        date.hour = 8
+        date.minute = 0
+        let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
+        
+        let identifier = "UniqueNotificationMerunas"
+        // You can setup multiple with different identifiers like so
+        // let identifier = UUID().uuidString
+        let request = UNNotificationRequest(identifier: identifier, content: notification, trigger: trigger)
+        current.add(request) { (error : Error?) in
+             if let theError = error {
+                 print("Found an error", theError)
+             }
+        }
+        
         return true
     }
 
